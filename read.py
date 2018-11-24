2078704a76c3e54bcca64abef75dcccafc451d53
Eric <eric@EricdeMacBook-Air.local>
2018-11-25 02:53:18 +0800

    训练数据中

--- read.py
+++ read.py
@@ -7,7 +7,9 @@ import datetime
 import os
 import sys
 
+
 def read_poems(poems_file='data/poems.txt'):
+
     poems=[]
     with open(poems_file,'r',encoding='utf-8') as f:
         #读取每一行
@@ -45,7 +47,8 @@ def read_poems(poems_file='data/poems.txt'):
     #print(count_table)
     words,t=zip(*count_table)
     #print(words)
-    #因为后面要把那些长度短的诗补上空格，所以把空格也加到我们的字符库中
+    #因为后面要把那些长度短的诗补上空格，所以
+    # 把空格也加到我们的字符库中
     words=words[:len(words)]+(' ',)
     #print(words)
     #这两个字典非常关键，神经网络无法识别字符，通过这两个字典可以完成字符与数值的双向转换
@@ -53,46 +56,157 @@ def read_poems(poems_file='data/poems.txt'):
     id_to_word=dict(zip(word_to_id.values(),word_to_id.keys()))
     #生成诗的向量，把所有诗的每一个字符都用特定的数字表示
     poems_vec=[[word_to_id[word] for word in poem] for poem in poems]
+    print(word_to_id)
     #print(poems_vec)
     #返回诗向量和两个字典
     return poems_vec,word_to_id,id_to_word
 
 
+def create_batch(batch_size,poems_vec,word_to_id):
 
-if __name__=='__main__':
-    poems_vec,word_to_id,id_to_word=read_poems()
-    #筛选出空格代表的数字
-    space=word_to_id.get(' ')
-    batch_size=64
-    #print(space)
-    input_size=output_size=len(word_to_id)
-    X=tf.placeholder(tf.int32,[batch_size,None])
-    Y=tf.placeholder(tf.int32,[batch_size,None])
+    n_chunk=len(poems_vec)//batch_size
+    x_batch=[]
+    y_batch=[]
+    for i in range(n_chunk):
+        start_index=i*batch_size
+        end_index=start_index+batch_size
+        batches=poems_vec[start_index:end_index]
+        length=max(map(len,batches))
+        x_data = np.full((batch_size, length), word_to_id.get(' '), np.int32)
+        for row in range(batch_size):
+            x_data[row,:len(batches[row])]=batches[row]
+        y_data=np.copy(x_data)
+        y_data[:,:-1]=x_data[:,1:]
+        x_batch.append(x_data)
+        y_batch.append(y_data)
+    return x_batch,y_batch
 
-    model_name='LSTM'
+
+def model(input_data,output_data,word_size):
+
     rnn_size=128
-    layer_num=2
+    num_layers=2
+    batch_size=64
+    learning_rate=0.01
 
+    outs={}
     cell_fun=tf.nn.rnn_cell.BasicLSTMCell
     cell=cell_fun(rnn_size,state_is_tuple=True)
+    cell=tf.nn.rnn_cell.MultiRNNCell([cell]*num_layers,state_is_tuple=True)
+    initial_state=cell.zero_state(batch_size,tf.float32)
+    embedding=tf.Variable(tf.random_uniform([word_size+1,rnn_size],-1.0,1.0))
+    inputs=tf.nn.embedding_lookup(embedding,input_data)
 
-    lstm_layers=tf.nn.rnn_cell.MultiRNNCell([cell]*layer_num,state_is_tuple=True)
-    initial_state=lstm_layers.zero_state(batch_size,tf.float32)
+    outputs,last_state=tf.nn.dynamic_rnn(cell,inputs,initial_state=initial_state)
+    outputs=tf.reshape(outputs,[-1,rnn_size])
 
-    #创建一个变量域
-    with tf.variable_scope('lstm'):
-        scale=tf.sqrt(3/((rnn_size+output_size)*0.5))
-        w=tf.get_variable('w',[rnn_size,output_size],
-                          initializer=tf.random_uniform_initializer(-scale,scale))
-        b=tf.get_variable('b',[output_size],
-                          initializer=tf.random_uniform_initializer(-scale,scale))
-        with tf.device('/cpu:0'):
-            embedding=tf.get_variable('embedding',[input_size,rnn_size])
-            inputs=tf.nn.embedding_lookup(embedding,X)
-    
-    outputs,last_state=tf.nn.dynamic_rnn(layer_num,inputs,
-                                         initial_state=initial_state,scope='lstm')
-    logists=tf.matmul(outputs,[-1,rnn_size])
-    pred=tf.nn.softmax(logits=logists)
+    w=tf.Variable(tf.truncated_normal([rnn_size,word_size+1]))
+    b=tf.Variable(tf.zeros(shape=[word_size+1]))
+    logits=tf.nn.bias_add(tf.matmul(outputs,w),b)
 
-    #logists,last_state,pred,initial_state
+    labels=tf.one_hot(tf.reshape(output_data,[-1]),depth=word_size+1)
+    loss=tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=labels)
+    mean_loss=tf.reduce_mean(loss)
+    opti=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(mean_loss)
+
+    outs['initial_state']=initial_state
+    outs['outputs']=outputs
+    outs['mean_loss']=mean_loss
+    outs['loss']=loss
+    outs['last_state']=last_state
+    outs['train'] = opti
+
+    return outs
+
+
+def train():
+
+    epochs=50
+
+    poems_vec, word_to_id, id_to_word = read_poems()
+    x_batch, y_batch = create_batch(batch_size=64, poems_vec=poems_vec, word_to_id=word_to_id)
+
+    batch_size = 64
+    learning_rate = 0.01
+
+    poems_vec, word_to_id, id_to_word = read_poems()
+    input_data=tf.placeholder(tf.int32,[batch_size,None])
+    output_data=tf.placeholder(tf.int32,[batch_size,None])
+
+    outs = model(input_data=input_data,output_data=output_data,word_size=len(id_to_word))
+
+    saver=tf.train.Saver(tf.global_variables())
+    init=tf.group(tf.global_variables_initializer(),tf.local_variables_initializer())
+
+    with tf.Session() as sess:
+        sess.run(init)
+        start_epoch=0
+        checkpoint=tf.train.latest_checkpoint('model/')
+
+        if checkpoint:
+            saver.restore(sess,checkpoint)
+            print('载入数据点{}'.format(checkpoint))
+            start_epoch += int(checkpoint.split('-')[-1])
+        print('开始训练')
+
+        try:
+            for epoch in range(start_epoch,epochs):
+                n = 0
+                n_chunk = len(poems_vec) // batch_size
+                for batch in range(n_chunk):
+                    loss, _, _ = sess.run([
+                        outs['mean_loss'],
+                        outs['last_state'],
+                        outs['train']
+                    ], feed_dict={input_data: x_batch[n], output_data: y_batch[n]})
+                    n += 1
+                    print('迭代周期: %d , 样本: %d , 损失函数: %.6f' % (epoch, batch, loss))
+                if epoch % 6 == 0:
+                    saver.save(sess, os.path.join('model/','model_prefix'), global_step=epoch)
+        except KeyboardInterrupt:
+            print('开始手动保存')
+            saver.save(sess, os.path.join('model/', 'model_prefix'), global_step=epoch)
+            print('手动保存了保存点{}.'.format(epoch))
+
+
+def predict(input_data,word_size):
+    rnn_size = 128
+    num_layers = 2
+
+    outs = {}
+    cell_fun = tf.nn.rnn_cell.BasicLSTMCell
+    cell = cell_fun(rnn_size, state_is_tuple=True)
+    cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers, state_is_tuple=True)
+    initial_state = cell.zero_state(1, tf.float32)
+    embedding = tf.Variable(tf.random_uniform([word_size + 1, rnn_size], -1.0, 1.0))
+    inputs = tf.nn.embedding_lookup(embedding, input_data)
+
+    outputs, last_state = tf.nn.dynamic_rnn(cell, inputs, initial_state=initial_state)
+    outputs = tf.reshape(outputs, [-1, rnn_size])
+
+    w = tf.Variable(tf.truncated_normal([rnn_size, word_size + 1]))
+    b = tf.Variable(tf.zeros(shape=[word_size + 1]))
+    logits = tf.nn.bias_add(tf.matmul(outputs, w), b)
+
+    pred=tf.nn.softmax(logits=logits)
+
+    outs['initial_state']=initial_state
+    outs['last_state']=last_state
+    outs['pred']=pred
+
+    return outs
+
+
+def write(begin_word):
+
+    batch_size=1
+    print('大诗人思考中...')
+    poems_vec, word_to_id, id_to_word = read_poems()
+
+    input_data=tf.placeholder(tf.int32,[batch_size,None])
+
+    outs=predict(input_data=input_data,word_size=len(id_to_word))
+
+
+if __name__=='__main__':
+    train()
